1ce8ec70d21ff4ee7bb2e2e36f1dd9218ecbaa6f
ayush2000mickey <56113584+ayush2000mickey@users.noreply.github.com>
2022-01-07 20:24:08 +0530

    initial commit

---
+++ src/Banner.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+
+import requests from "./requests";
+
+import "./Banner.css";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ],
+      );
+      return request;
+    };
+
+    fetchData();
+  }, []);
+
+  function truncateString(str, num) {
+    return str?.length > num ? str.slice(0, num - 1) + "..." : str;
+  }
+
+  console.log(movie);
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_content">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+
+        <h1 className="banner_description">
+          {truncateString(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner__fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
